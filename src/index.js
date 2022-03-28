b47e9f396ee974bf3c06cabda45d4477d945df7b
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-28 23:02:27 +0000

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App';
+import { App } from './App';
 import './index.css';
 
 ReactDOM.render(
